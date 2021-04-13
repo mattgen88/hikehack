6bfd3b1c25d1e61bbcc09b6b1a617b2ddc737914
Matthew General <matthew.general@gmail.com>
2021-04-13 14:37:09 -0400

    add auth0

--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import App from './App.vue'
 import router from './router'
 import { Icon } from "leaflet";
 import 'leaflet/dist/leaflet.css';
+// Import the Auth0 configuration
+import { domain, clientId } from "../auth_config.json";
+
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
 delete Icon.Default.prototype._getIconUrl;
 
